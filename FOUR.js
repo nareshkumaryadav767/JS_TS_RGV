101991b9481ea48a616e80254c5ab40c59ac3975
JENNE NARESH <nareshkumaryadav7671@gmail.com>
2024-07-19 15:27:10 +0530

    Add files via upload

---
+++ FOUR.js
@@ -0,0 +1,16 @@
+let eid=101
+let ename="Naresh"
+let avail=true;
+let loc;
+let emails=["nareshkumaryadav7671@gmail.com"]
+
+let detail={ esal:45000,
+              doj:'12/03/2024'
+            }
+
+console.log(typeof eid)           
+console.log(typeof ename)
+console.log(typeof avail)
+console.log(typeof loc)
+console.log(typeof email)
+console.log(typeof detail)
---
+++ TWO.js
@@ -0,0 +1,10 @@
+/* let sizes=38,40,42,44; */
+
+var sizes=[38,40,42,44];
+console.log(sizes)
+console.log(typeof sizes)
+
+
+var emp={ eid:101, name:"naresh"}
+console.log(emp)
+console.log(typeof emp)
